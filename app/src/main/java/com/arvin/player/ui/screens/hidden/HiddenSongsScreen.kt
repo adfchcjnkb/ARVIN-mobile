570a73f7fa8a8e322bb7a38c63fc08a6a52b5550
adfchcjnkb <arvinkheradmand28@gmail.com>
2026-07-18 01:02:59 +0330

    Fix: add missing dp import in HiddenSongsScreen

--- app/src/main/java/com/arvin/player/ui/screens/hidden/HiddenSongsScreen.kt
+++ app/src/main/java/com/arvin/player/ui/screens/hidden/HiddenSongsScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.arvin.player.R
 import com.arvin.player.data.repository.MusicRepository
